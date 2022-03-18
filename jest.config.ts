3639739f574d90e768c2fef8775297127880e061
raphael.jorge <27439414+raphael-jorge@users.noreply.github.com>
2022-03-18 14:58:12 -0300

    refactor: upgrade jest setup according to next 12 recomendations

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import nextJest from 'next/jest';
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
+
+const createJestConfig = nextJest({ dir: './' });
+
+const customJestConfig: InitialOptionsTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@enums/(.*)$': '<rootDir>/src/enums/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@view-models/(.*)$': '<rootDir>/src/view-models/$1',
+    '^@test-utils/(.*)$': '<rootDir>/tests/.utils/$1',
+  },
+};
+
+export default createJestConfig(customJestConfig);
